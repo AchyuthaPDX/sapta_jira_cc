c7933727687526b0eb07ee8d49bead86a04d1772
ArpithaSrinivas <89316938+ArpithaSrinivas5@users.noreply.github.com>
2022-02-21 21:42:41 -0800

    Jira Connector logic to create jira issue

---
+++ jira/backend/index.js
@@ -0,0 +1,49 @@
+//connector logic  to JIRA API
+
+const express = require("express");
+const app = express();
+const cors = require("cors")
+var JiraClient = require("jira-connector");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+var jira = new JiraClient({
+    host: "test1234566.atlassian.net",
+    basic_auth: {
+        email: "codepythongo@gmail.com",
+        api_token: "pWLXx5a4gYXiBHuIBM1ND583",
+    },
+});
+
+app.post("/", (req, res) => {
+    res.send("starts new nodejs project");
+
+    if (req.body.status == "success") {
+        jira.issue.createIssue({
+            fields: {
+                project: {
+                    key: "SI",
+                },
+                summary: "Testing JIRA rest api",
+                description: " this issue is create from JIRA rest api connector",
+                issuetype: {
+                    name: "story",
+                },
+            },
+            function(error, issue) {
+                console.log("error", error);
+                console.log("issue", issue);
+
+            }
+        });
+    } else {
+        console.log("status: not success")
+    }
+
+});
+
+
+app.listen(5000, () => console.log("listing on port 5000"));
